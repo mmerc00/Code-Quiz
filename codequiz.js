d2a272193f5345db63336ce355e9d2ba96ecff5a
Melissa Mercado <melissa.a.merc@gmail.com>
2020-07-05 21:11:31 -0400

    psuedo code

---
+++ codequiz.js
@@ -0,0 +1,43 @@
+//GIVEN I am taking a code quiz
+
+//WHEN I click the start button
+
+//THEN a timer starts and I am presented with a question
+
+//WHEN I answer a question
+
+//THEN I am presented with another question
+
+//WHEN I answer a question incorrectly
+
+//THEN time is subtracted from the clock
+
+//WHEN all questions are answered or the timer reaches 0
+
+//THEN the game is over
+
+//WHEN the game is over
+
+//THEN I can save my initials and score//
+//variables
+//score counter
+//timer
+//objects
+
+//click start button
+
+//the timer starts and score counter
+
+//first questions pops up user input
+
+//IF  first question correct move on
+
+//ELSE first question wrong time is subtractes
+
+// IF when the timer is out its game over
+
+//ELSE IF when all questions are over game is over
+
+//save score
+
+//return to start

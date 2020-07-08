7d2929ec10bd74c9a271dd3057c9b6b2dea39c80
Melissa Mercado <melissa.a.merc@gmail.com>
2020-07-08 08:26:22 -0400

    changed syntax and layout worked in group

--- codequiz.js
+++ codequiz.js
@@ -1,103 +1,85 @@
-//variables
-//obejcts and folder
-var questionAndAnswer = [
-  {
-    question: "1. Where was the jersey shore filmed?",
-    options: ["LBI, NJ", "Seaside Heights, NJ", "Cape May, NJ"],
-    correct: 1,
-  },
-  {
-    question: "2. Jenny's nick-name on the show was?",
-    options: ["Jmoney", "J420", "Jwow"],
-    correct: 2,
-  },
-  {
-    question: "3. Where did the cast work while in Seaside Heights?",
-    options: ["Bakery", "T-shirt Shop", "Pizzeria"],
-    correct: 1,
-  },
-];
-var qIndex = 0;
-var secondsLeft = 100;
+//Starting/init Data
+//declaring variables
+var timerEl = document.querySelector(".timer");
+var startEl = document.querySelector("#start");
+var optionA = document.querySelector("#optionA");
+var optionB = document.querySelector("#optionB");
+var optionC = document.querySelector("#optionC");
+var optionD = document.querySelector("#optionD");
+//Array of Options
+var options = [optionA, optionB, optionC, optionD];
+var question = document.querySelector("#question");
+var secondsLeft = 10;
 var score = 0;
-var timerInterval;
+//Questions in an array
+var quiz =
+  // Q1
+  {
+    q1: "Where in NJ was the Jersey Shore filmed?",
+    a1: "LBI",
+    b1: "Seaside Heights",
+    c1: "Cape May",
+    d1: "Point Pleasant",
+    //  Q2
+    q2: "Jenny's name on the show was",
+    a2: "Jwow",
+    b2: "Jmoney",
+    c2: "Jshore",
+    d2: "have no idea",
+    // Q3
+    q3: "Where did the cast work while filiming in NJ",
+    a3: "Bakery",
+    b3: "T-shirt shop",
+    c3: "Pizzera",
+    d3: "Tatoo shop",
+  };
+//Event Click first Function
+// var hOne = document.createElement("h1")
+// hOne.textContent = "This is the hOne element"
+// document.body.append(hOne)
+// hOne.setAttribute("style", "text-align:center")
+var displayQuestions = function () {
+  optionA.textContent = quiz.a1;
+  document.append(optionA);
+};
+displayQuestions;
+// console.log(displayQuestions);
+//Array of Answers
 
-//console.log(questionAndAnswer[0].options);
-
-// console.log(questionAndAnswer[1].question);
-// console.log(questionAndAnswer[1].options);
-
-// console.log(questionAndAnswer[2].question);
-// console.log(questionAndAnswer[2].options);
-
-// for (var i = 0; i < questionAndAnswer.length; i++) {
-//   console.log(questionAndAnswer[i].question);
-//   console.log(questionAndAnswer[i].options);
-//   for (var k = 0; k < questionAndAnswer[i].options.length; k++) {
-//     console.log(questionAndAnswer[i].options[k]);
-//   }
-//   console.log(questionAndAnswer[i].correct);
-// }
-
-//timer
-var timeEl = document.querySelector(".timer");
-
-var mainEl = document.getElementById("main");
-var questionsEl = document.getElementById("questions");
-
-displayQuestion();
-
-function displayQuestion() {
-  questionsEl.innerHTML = "";
-
-  var question = document.createElement("h1");
-  question.textContent = questionAndAnswer[qIndex].question;
-
-  questionsEl.appendChild(question);
-}
-
-function setTime() {
-  clearInterval(timerInterval);
-
-  timerInterval = setInterval(function () {
+var answers = ["Seaside Heights", "Jwow", "T-shirt Shop"];
+//User interactions
+//When we click on Start, timer countdown starts
+startEl.addEventListener("click", function () {
+  startEl.style.display = "none";
+  var myInterval = setInterval(function () {
+    timerEl.textContent = secondsLeft;
     secondsLeft--;
-    timeEl.textContent = secondsLeft;
-
-    if (secondsLeft <= 0) {
-      clearInterval(timerInterval);
+    if (secondsLeft === -1) {
+      clearInterval(myInterval);
+      displayQuestions;
     }
-  }, 1 * 1000);
-}
+  }, 1000);
+});
 
-//if statements
+// when first question is displayed timer starts
 
-//objects
-
-//click start button
-
-//the timer starts and score counter
-
-//first questions pops up user input
-
-//IF  first question correct move on
-
-//ELSE first question wrong time is subtractes
-
-// IF when the timer is out its game over
-
-//ELSE IF when all questions are over game is over
-
-//save score
-
-//return to start
-
-function sendMessage() {
-  timeEl.textContent = " ";
-
-  var imgEl = document.createElement("img");
-
-  // imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
-}
-
-setTime();
+//call on some DOMS
+//Handler Functions===============================================================================
+// Check answer - use answer array and .include
+//if it is correct
+//alert yay!
+//then move to the next question
+// Else if it is wrong
+// alert wrong,
+//ten seconds taken away,
+// move to the next question
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//Display Input===============================================================================
+//WHEN the game is over
+//Clear screen
+//Reset Timer
+//Submit Button appears
+//THEN I can save my initials and score
+//When click submit button
+//Return to the beginning of quiz
